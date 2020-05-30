083466b2d5b0e69df2ebd5c8abbb89172c40e96b
gauravdwivedi <gauravdwivedi@hotmail.com>
2020-05-30 21:25:54 +0530

    smooth scroll added

--- script.js
+++ script.js
@@ -1,3 +1,40 @@
+var navMenuAnchorTags =document.querySelectorAll('.nav-menu a');
+
+
+for(var i=0;i<navMenuAnchorTags.length;i++){
+
+        navMenuAnchorTags[i].addEventListener('click',function(event){
+            event.preventDefault();
+
+            var targetSectionID = this.textContent.trim().toLowerCase();
+
+                console.log(targetSectionID);
+            var targetSection =document.getElementById(targetSectionID);
+
+                console.log(targetSection);
+
+            
+            
+            
+               
+            var interval =setInterval(function(){
+                var targetSectionCoordinates = targetSection.getBoundingClientRect();
+
+                if(targetSectionCoordinates.top <= 0){
+                    console.log(targetSectionCoordinates.top);
+                    clearInterval(interval);
+                    return;
+                }
+                window.scrollBy(0,50);
+            },20);
+        });
+}
+
+
+
+
+
+
 var progressBars = document.querySelectorAll(".skill-progress > div");
 var skillsContainer = document.getElementById('skills-container');
 var animationDone = false;
